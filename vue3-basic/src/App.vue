627015d6be489660060ec6c0055faddaad21c189
yashoku <70477548+yashoku233@users.noreply.github.com>
2023-02-25 22:59:01 +0800

    2
    232

--- vue3-basic/src/App.vue
+++ vue3-basic/src/App.vue
@@ -1,43 +1,58 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <h1>{{count}}</h1>
-  <h1>{{double}}</h1>
-  <h1>X:{{x}}</h1>
-  <h1>Y:{{y}}</h1>
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <h1>X:{{ x }}</h1>
+  <h1>Y:{{ y }}</h1>
+  <h1 v-if="loading"> </h1>
+  <img v-if="loaded" :src="result.message">
   <button @click="increase"> 👍+1</button>
   <button @click="updateGreeting"> Updata</button>
 </template>
 
 <script lang="ts">
 /* eslint-disable */
-import {ref, computed, reactive, toRefs, onMounted, onUnmounted} from 'vue';
+import { ref, computed, reactive, toRefs, onMounted, onUnmounted, watch } from 'vue';
 import useMousePositions from './hook/useMousePositions'
 import useURLloader from './hook/useLoader'
+import { jsxClosingElement } from '@babel/types';
+
 interface DataProps {
-  count:number,
+  count: number,
   double: number,
   increase: () => void,
-  numbers:number[];
-  person: {name?:string},
+  numbers: number[];
+  person: { name?: string },
+}
+interface DogResult {
+  result: string,
+  status: string,
+  message: string,
 }
 export default {
   name: 'App',
-  setup(){
-    const data: DataProps  = reactive({
-      count:0,
-      double: computed(() => data.count *2),
-      increase: ()=> {
+  setup() {
+    const data: DataProps = reactive({
+      count: 0,
+      double: computed(() => data.count * 2),
+      increase: () => {
         data.count++
       },
-      numbers:[0, 1, 3],
-      person:{name:'zjw'},
+      numbers: [0, 1, 3],
+      person: { name: 'zjw' },
     })
     const greetings = ref('');
     const updateGreeting = () => {
       greetings.value += 'Hello!'
     }
-    const {loaded, loading, result} = useURLloader('htps://dog.eco/api/breeds/imange/random');
-    const {x, y} = useMousePositions();
+
+    const { loaded, loading, result } = useURLloader<DogResult>('https://dog.ceo/api/breeds/image/random');
+      watch(result, () => {
+      if (result.value){
+        console.log(result.value.message)
+      }
+    })
+    const { x, y } = useMousePositions();
     const refData = toRefs(data);
     return {
       ...refData,
--- vue3-basic/src/hook/useLoader.ts
+++ vue3-basic/src/hook/useLoader.ts
@@ -1,19 +1,26 @@
 import { ref } from 'vue';
 import axios from 'axios'
 
-function useURLLoader (url: string) {
-    const result = ref(null);
+function useURLLoader<T> (url: string) {
+    const result = ref<T | null>(null);
     const loading =ref(true);
     const loaded = ref(false);
     const error  = ref(null);
-    axios.get(url).then((rawData) => {
+    
+    axios.get(url).then((rawData) =>{
         loading.value = false;
-        loaded.value =true;
-        result.value = rawData.data
-    }).catch((e) =>{
-        error.value = e
+        loaded.value = true;
+        result.value = rawData.data;
+    }).catch(e =>{
+        error.value = e;
+        loading.value = false;
     })
-    return {result, loading,loaded,error}
+    return {
+        result,
+        loading,
+        loaded,
+        error,
+    }
 }
 
 export default useURLLoader 
