e423f570e3a4aac826e57a746d64245893ed46c8
yashoku101 <jiwang.zhang@ruhlamat-st.com.cn>
2023-04-07 13:02:02 +0800

    uspense

---
+++ vue3-basic/src/components/asyncShow.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{result}}</h1>
+</template>
+
+<script>
+export default {
+  setup() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          result: 42,
+        });
+      }, 3000);
+    });
+  },
+};
+</script>
+
+<style>
+</style>
