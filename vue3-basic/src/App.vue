3fc78419ca91c473d9b126acb2be9ff6e9c03908
yashoku101 <jiwang.zhang@ruhlamat-st.com.cn>
2023-04-11 10:39:26 +0800

    2333

--- vue3-basic/src/App.vue
+++ vue3-basic/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
   <h1>{{ count }}</h1>
   <h1>{{ countcopy }}</h1>
   <h1>{{ double }}</h1>
@@ -13,82 +13,82 @@
       <h1>Loading!....</h1>
     </template>
   </suspense>
-  <modal :isOpen="openModal" @close-modal="onModalClose">
-    my modal
-  </modal>
-  <h1 v-if="loading"> </h1>
-  <img v-if="loaded" :src="result.message">
-  <button @click="increase"> 👍+1</button>
-  <button @click="updateGreeting"> Updata</button>
+  <modal :isOpen="openModal" @close-modal="onModalClose">my modal</modal>
+  <h1 v-if="loading"></h1>
+  <img v-if="loaded" :src="result.message" />
+  <button @click="increase">👍+1</button>
+  <button @click="updateGreeting">Updata</button>
 </template>
 
 <script lang="ts">
 /* eslint-disable */
 import { ref, computed, reactive, toRefs, onMounted, onUnmounted, watch } from 'vue';
-import useMousePositions from './hook/useMousePositions'
-import useURLloader from './hook/useLoader'
-import useModel from './components/useModel.vue'
-import dogShow from './components/dogShow.vue'
-import Modal from './components/modal.vue'
+import useMousePositions from './hook/useMousePositions';
+import useURLloader from './hook/useLoader';
+import useModel from './components/useModel.vue';
+import dogShow from './components/dogShow.vue';
+import Modal from './components/modal.vue';
 
 interface DataProps {
-  count: number,
-  double: number,
-  increase: () => void,
+  count: number;
+  double: number;
+  increase: () => void;
   numbers: number[];
-  person: { name?: string },
+  person: { name?: string };
 }
 interface DogResult {
-  result: string,
-  status: string,
-  message: string,
+  result: string;
+  status: string;
+  message: string;
 }
-interface CatResult{
-  url:string,
-  id:string,
-  width:number,
-  height:number,
+interface CatResult {
+  url: string;
+  id: string;
+  width: number;
+  height: number;
 }
 export default {
   name: 'App',
-   components: {
-   useModel,
-   dogShow,
-   Modal,
+  components: {
+    useModel,
+    dogShow,
+    Modal,
   },
   setup() {
     const data: DataProps = reactive({
       count: 0,
       double: computed(() => data.count * 2),
       increase: () => {
-        data.count++
+        data.count++;
       },
       numbers: [0, 1, 3],
       person: { name: 'zjw' },
-    })
+    });
     const datacopy = reactive({
       countcopy: 0,
       doublecopy: computed(() => data.count * 2),
       increase: () => {
-        data.count++
+        data.count++;
       },
       numbers: [0, 1, 3],
       person: { name: 'zjw' },
-    })
+    });
     const greetings = ref('');
     const updateGreeting = () => {
-      greetings.value += 'Hello!'
-    }
+      greetings.value += 'Hello!';
+    };
     const openModal = ref(true);
-    const onModalClose = () =>{
+    const onModalClose = () => {
       openModal.value = false;
-    }
-    const { loaded, loading, result } = useURLloader<CatResult[]>('https://dog.ceo/api/breeds/image/random');
+    };
+    const { loaded, loading, result } = useURLloader<CatResult[]>(
+      'https://dog.ceo/api/breeds/image/random',
+    );
     watch(result, () => {
       if (result.value) {
-        console.log(result.value[0].url)
+        console.log(result.value[0].url);
       }
-    })
+    });
     const { x, y } = useMousePositions();
     const refData = toRefs(data);
     const refDataCopy = toRefs(datacopy);
@@ -104,9 +104,9 @@ export default {
       result,
       openModal,
       onModalClose,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style>
--- vue3-basic/src/components/modal.vue
+++ vue3-basic/src/components/modal.vue
@@ -14,7 +14,7 @@ export default defineComponent({
   props: {
     isOpen: Boolean,
   },
-  setup(context) {
+  setup(props,context) {
     const buttonClick = () => {
       context.emit('close-modal');
     };
